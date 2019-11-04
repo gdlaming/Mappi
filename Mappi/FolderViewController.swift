5beeb732f40b38ff49b1d34d8f6f7dd77dcbb9fa
Gillian <gdlaming3@gmail.com>
2019-11-04 11:37:48 -0600

    view controllers being added

---
+++ Mappi/FolderViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FolderViewController.swift
+//  Mappi
+//
+//  Created by Gillian Dibbs Laming on 11/4/19.
+//  Copyright © 2019 cse438. All rights reserved.
+//
+
+import UIKit
+
+class FolderViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
