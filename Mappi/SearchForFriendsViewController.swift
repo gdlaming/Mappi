ff36425600240e3b16a2bb5c937221b500fb57fe
Gillian <gdlaming3@gmail.com>
2019-11-28 12:44:46 -0500

    working on searching functionality

--- Mappi/SearchForFriendsViewController.swift
+++ Mappi/SearchForFriendsViewController.swift
@@ -8,22 +8,53 @@
 
 import UIKit
 
-class SearchForFriendsViewController: UIViewController, UISearchBarDelegate{
-
-    @IBOutlet weak var searchBar: UISearchBar!
-    var query = ""
+class SearchForFriendsViewController: UIViewController, UISearchBarDelegate, UITableViewDelegate, UITableViewDataSource{
     
+    
+    
+    @IBOutlet weak var friendView: UITableView!
+    @IBOutlet weak var searchBar: UISearchBar!
+    var myArray = [String]()
+    
+    var query = "search for your friends here"
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        myArray.append("Gillian")
         self.searchBar.delegate = self
+        friendView.dataSource = self
+        friendView.delegate = self
+        //searchBar.returnKeyType = UIReturnKeyType.done
+        
         // Do any additional setup after loading the view.
     }
     
-    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String){
-        query = searchBar.text!
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1;
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let myCell = friendView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath) as! UITableViewCell
+        //        let myCell = friendView.dequeueReusableCell(withIdentifier: "theCell")! as! FriendTableViewCell
+       // print("setting text")
+        myCell.textLabel!.text = myArray[indexPath.row]
+        //print("set text")
+        //myCell.textLabel!.text = "\(indexPath.section) Row:\(indexPath.row)"
+        return myCell
+    }
+    
+
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        //let query = searchBar.text!
+        print("hello, world!")
         fetchDataFromSearch()
     }
+    
+    
+//    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String){
+//        query = searchBar.text!
+//        fetchDataFromSearch()
+//    }
     
     func fetchDataFromSearch(){
         print(query)
