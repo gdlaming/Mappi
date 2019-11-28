9327844bd9807439e3a5fed1580fa26f5592353d
Gillian <gdlaming3@gmail.com>
2019-11-28 14:04:22 -0500

    adding cell info

---
+++ Mappi/AddFriendTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  AddFriendTableViewCell.swift
+//  Mappi
+//
+//  Created by Gillian Dibbs Laming on 11/28/19.
+//  Copyright © 2019 cse438. All rights reserved.
+//
+
+import UIKit
+
+class AddFriendTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
