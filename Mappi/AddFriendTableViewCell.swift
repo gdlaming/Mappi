9fdeea56d8d29ff5cc01a9128b0cd05313586480
Gillian <gdlaming3@gmail.com>
2019-11-29 22:54:16 -0500

    going to eat some thanksgiving dinner

--- Mappi/AddFriendTableViewCell.swift
+++ Mappi/AddFriendTableViewCell.swift
@@ -26,19 +26,40 @@ class AddFriendTableViewCell: UITableViewCell {
 
     @IBAction func addFriend(_ sender: Any) {
         print("adding friend")
-        //UserDefaults.standard.set(id, forKey: "id")
-       let id = UserDefaults.standard.string(forKey: "id")!
-       /* print("my id \(id)")
-        let first = myArray[indexPath.row].split(separator: " ")
-        let last = first[1]
-        print("first \(first) last \(last)")
-        let query = "select userID from users where firstName=?"
-        let f = try folderDB.executeQuery(query, values: [first])
-        while(f.next()){
-            friendID = Int(f.int(forColumn: "userID"))
-            print("friend id: \(friendID)")
+        var friendID: Int
+        let thepath = Bundle.main.path(forResource: "mappi", ofType: "db")
+        let DB = FMDatabase(path: thepath)
+        
+        if !(DB.open()) {
+            print("Unable to open database")
+        } else {
+            do{
+                let id = UserDefaults.standard.string(forKey: "id")!
+                //print("my id \(id)")
+                let names = name.text!.split(separator: " ")
+                let first = names[0]
+                let last = names[1]
+                var query = "select userID from users where firstName=?"
+                let f = try DB.executeQuery(query, values: [first])
+                while(f.next()){
+                    friendID = Int(f.int(forColumn: "userID"))
+                    print("friend id: \(friendID)")
+                    addFriendship(friendID, id)
+                }
+                
+            }
+            catch let error as NSError {
+                print("failed \(error)")
+                
+            }
         }
- */
+        //now, just need to
+       
+ 
 
+    }
+    
+    func addFriendship(_ friendID: Int, _ id: String){
+        print("adding friendship")
     }
 }
